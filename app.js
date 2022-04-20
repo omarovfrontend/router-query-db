145d7b130d5c0b851214274dea1ba1f11d573fc6
Shamil Omarov <s_omarov05@mail.ru>
2022-04-21 01:33:17 +0300

    Добавили стили, статический метод, + завершили приложение!

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const PORT = 3000;
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'));  //дает возможность получить любой файл из этой папки
 app.use(express.json());
 app.use(express.urlencoded({extends: true}));
 
--- routers/posts.js
+++ routers/posts.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { Post } = require('../db/models');
+const { Post, User } = require('../db/models');
 
 router.get('/all', async (req, res) => {
   try {
     const posts = await Post.findAll({raw: true});
-    res.render('posts', { posts });
+    res.render('posts', { Posts: posts });
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+});
+
+// /posts/name => полный путь
+router.get('/', async (req, res) => {
+  const {name} = req.query;
+  try {
+    const userPost = (await User.findOne({where: {name}, include: [Post]})).dataValues;
+    userPost.Posts = userPost.Posts.map((el) => el.dataValues);
+    res.render('posts', { Posts: userPost.Posts, name: userPost.name });
   } catch (error) {
     console.log(error);
     res.status(500).end();
